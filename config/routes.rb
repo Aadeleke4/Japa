a52d666a4a9bc07bd9b9225a8c18620c09b4298a
adetumi <tumiadeleke@gmail.com>
2024-03-30 20:16:08 -0500

    carts controler added

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+
   # Defines the root path route ("/")
   root "home#index"
 
@@ -20,5 +21,8 @@ Rails.application.routes.draw do
     root to: "admin#index", as: :admin_root
   end
   
+  resources :categories, only: [:show]
+  resources :products, only: [:show]
   get "admin" => "admin#index"
+  
 end
