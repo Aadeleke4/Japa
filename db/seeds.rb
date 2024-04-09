833369e02806cb2322e2ec8805e40fd56e8bcd57
adetumi <tumiadeleke@gmail.com>
2024-04-09 14:47:22 -0500

    added seeds for 100 products

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,28 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# db/seeds.rb
+
+require 'faker'
+
+# Create categories
+categories = ['Sweat Shirt', 'Belt', 'Accesories', 'Hats']
+
+categories.each do |category_name|
+  Category.create(name: category_name)
+end
+
+# Create products
+100.times do
+  name = Faker::Commerce.product_name
+  description = Faker::Lorem.sentence
+  price = Faker::Commerce.price(range: 50..500.0)
+  category_id = rand(1..4)
+
+  Product.create(
+    name: name,
+    description: description,
+    price: price,
+    category_id: category_id
+  )
+end
